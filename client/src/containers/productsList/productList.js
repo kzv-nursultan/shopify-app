40bcfb6163ea7ca1ecef8ec3833591a8e23f6d8e
kzv-nursultan <kazievnursultan5554@gmail.com>
2024-01-30 21:38:49 +0600

    added redux state

--- client/src/containers/productsList/productList.js
+++ client/src/containers/productsList/productList.js
@@ -1,27 +1,29 @@
-import React, { useEffect, useState } from "react";
-import axiosInst from "../../configs/axiosInst";
-import { Card } from "../../components";
+import { useEffect } from "react";
 import "./productList.css";
+import { Card } from "../../components";
+import axiosInst from "../../configs/axiosInst";
+import { useDispatch, useSelector } from "react-redux";
+import { addProducts } from "../../configs/redux/actions";
 
 const ProductList = () => {
-  const [state, setState] = useState([]);
-
-  const fetchProducts = async () => {
-    try {
-      const { data } = await axiosInst.get("/products");
-      setState(data);
-    } catch (e) {
-      console.error(e);
-    }
-  };
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.main.products);
 
   useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data } = await axiosInst.get("/products");
+        dispatch(addProducts(data));
+      } catch (e) {
+        console.error(e);
+      }
+    };
     fetchProducts();
-  }, []);
-
+  }, [dispatch]);
+  
   return (
     <div className="container">
-      {state.map((product) => (
+      {products?.map((product) => (
         <Card key={product._id} {...product.node} _id={product._id} />
       ))}
     </div>
